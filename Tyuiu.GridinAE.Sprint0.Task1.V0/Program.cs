3414ec024eb940a61834fafcffcddd3beaea6beb
Артём Гридин <Tyuiu.GridinAE@outlook.com>
2024-09-13 16:07:38 +0500

    Task1

---
+++ Tyuiu.GridinAE.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Tyuiu.GridinAE.Sprint0.Task1.V0;
+
+class Program
+{
+    static void Main()
+    {
+        string name = "Артём";
+        int age = 17;
+
+        Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + " лет.");
+        Console.ReadKey();
+    }
+}
