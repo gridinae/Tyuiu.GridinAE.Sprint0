836dd0277ff6b6b2d20436fb6ee844399ed5fbea
Артём Гридин <Tyuiu.GridinAE@outlook.com>
2024-09-17 17:19:38 +0500

    Task4

---
+++ Tyuiu.GridinAE.Sprint0.Task4.V0.Lib/DataService.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.GridinAE.Sprint0.Task4.V0.Lib
+{
+    public class DataService
+    {
+        public static int Addition(int a, int b) => a + b;
+        public static int Subtraction(int a, int b) => a - b;
+        public static int Multiplication(int a, int b) => a * b;
+        public static double Division(int a, int b) => a / b;
+    }
+}
