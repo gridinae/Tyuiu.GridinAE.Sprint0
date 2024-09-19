da8e0c5ec6db3f155a058d154c630ed3e68383cf
Артём Гридин <Tyuiu.GridinAE@outlook.com>
2024-09-19 21:41:14 +0500

    Task5

---
+++ Tyuiu.GridinAE.Sprint0.Task5.V0.Lib/DataService.cs
@@ -0,0 +1,21 @@
+﻿namespace Tyuiu.GridinAE.Sprint0.Task5.V0.Lib;
+
+public class DataService
+{
+
+    public static int Addition(int a, int b) => a + b;
+    public static int Subtraction(int a, int b) => a - b;
+    public static int Multiplication(int a, int b) => a * b;
+    public static double Division(int a, int b)
+    {
+        if (b == 0)
+        {
+            Console.WriteLine("Переменная b = 0, на ноль делить нельзя.");
+            return -1;
+        }
+        else
+        {
+            return a / b;
+        }
+    }
+}
---
+++ Tyuiu.GridinAE.Sprint0.Task5.V0.Test/DataServiceTest.cs
@@ -0,0 +1,30 @@
+namespace Tyuiu.GridinAE.Sprint0.Task5.V0.Test;
+using Tyuiu.GridinAE.Sprint0.Task5.V0.Lib;
+
+[TestClass]
+public class DataServiceTest
+{
+    [TestMethod]
+    public void CheckAdditionValid()
+    {
+        Assert.AreEqual(10, DataService.Addition(5, 5));
+    }
+
+    [TestMethod]
+    public void CheckSubtractionValid()
+    {
+        Assert.AreEqual(5, DataService.Subtraction(10, 5));
+    }
+
+    [TestMethod]
+    public void CheckMultiplicationValid()
+    {
+        Assert.AreEqual(50, DataService.Multiplication(10, 5));
+    }
+
+    [TestMethod]
+    public void CheckDivisionValid()
+    {
+        Assert.AreEqual(3, DataService.Division(9, 3));
+    }
+}
